36e73acf433eb28eca9335d5442adcb3c41b1a8a
zuckeyM-17 <k.matsumura0314@gmail.com>
2020-12-29 17:03:54 +0900

    sign_in page

--- src/app/routes/index.go
+++ src/app/routes/index.go
@@ -7,9 +7,18 @@ import (
 )
 
 func indexRoutes(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "this is title",
-		})
+	r.GET("/", getIndex)
+	r.GET("/sign_in", getSignin)
+}
+
+func getIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "this is title",
+	})
+}
+
+func getSignin(c *gin.Context) {
+	c.HTML(http.StatusOK, "signin.html", gin.H{
+		"title": "this is sign_in page",
 	})
 }
